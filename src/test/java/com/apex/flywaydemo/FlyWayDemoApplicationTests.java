120dc24227b527b5de2aff812b4d674977269ac7
Christian A. De La Torre L <kytttyan@gmail.com>
2022-06-17 12:35:18 -0500

    First Commit

---
+++ src/test/java/com/apex/flywaydemo/FlyWayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.flywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyWayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
