bb334bf8a3704e7dd4d77df69db8fc35f89b0ce7
Christian A. De La Torre L <kytttyan@gmail.com>
2022-06-20 14:58:46 -0500

    Change name of table

--- src/main/resources/db/migration/V2__Create_clients_table.sql
+++ src/main/resources/db/migration/V2__Create_clients_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `Apex`.`Consultants` (
+CREATE TABLE `Apex`.`consultants` (
   `idConsultants` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `lastName` VARCHAR(45) NOT NULL,
   `age` INT NOT NULL,
-  `client` INT NULL DEFAULT NULL,
+  `clientId` INT NULL DEFAULT NULL,
   PRIMARY KEY (`idConsultants`));
 
